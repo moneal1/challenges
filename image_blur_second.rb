4bcd77cef788b1481dfda238da44fed8acd07142
Mike O'Neal <MikeONeal18@gmail.com>
2019-09-27 02:58:38 +0000

    Image blur 2

--- image_blur_second.rb
+++ image_blur_second.rb
@@ -6,35 +6,32 @@ class Image
   
     def get_ones
       ones = []
-      @show.each_with_index do |row, row_number|
-        row.each_with_index do |item, col_number|
+      @show.each_with_index do |x, x_number|
+        x.each_with_index do |item, y_number|
           if item == 1
-            ones << [row_number, col_number]
-          end
+            ones << [x_number, y_number]
+            
+                  end
         end
       end
-      
+     ones 
     end
     def row
     end
-    def x_number
-        @x_number = row
-    end
-    def y_number
-        @y_number = col
-    end
+    
   
     def blur!
       ones = get_ones
-    @show.each_with_index do |x, x_number|
-     x.each_with_index do |item, y_number|
-     ones.each do |x_number, y_number|
-  
-         if x_number == x_number && y_number == y_number
-         @show[x_number -1][y_number] = 1 unless x_number == 0
-        @show[x_number +1][y_number] = 1 unless x_number >= 3 
-         @show[x_number][y_number -1] = 1 unless y_number == 0 
-         @show[x_number][y_number +1] = 1 unless y_number >= 3 
+
+      @show.each_with_index do |x, current_x_number|
+      x.each_with_index do |item, current_y_number|
+      ones.each do |x_number, y_number|
+        
+     if x_number == current_x_number && y_number == current_y_number
+         @show[current_x_number -1][current_y_number] = 1 unless current_x_number == 0
+         @show[current_x_number +1][current_y_number] = 1 unless current_x_number >= 3 
+         @show[current_x_number][current_y_number -1] = 1 unless current_y_number == 0 
+         @show[current_x_number][current_y_number +1] = 1 unless current_y_number >= 3 
               end
             end
           end
