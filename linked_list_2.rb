ca1c9bcec0af159f777fa5dc1595c8b847fc42e3
Mike O'Neal <MikeONeal18@gmail.com>
2019-10-11 20:50:57 +0000

    linked_list_2

--- linked_list_2.rb
+++ linked_list_2.rb
@@ -88,7 +88,7 @@ end
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
-
+node1.next_node = node3
 print_values(node3)
 puts ""
 revlist = reverse_list(node3)
